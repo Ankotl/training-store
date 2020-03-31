b88115335cc831c4e5db7abef78145fd9e0db01a
anton_kotl <llluzuk@mail.ru>
2020-03-31 08:11:38 +0300

    Add Models ProductsApp

--- store/catalog/models.py
+++ store/catalog/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Категория', max_length=50)
+    description = models.TextField('Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField('Товар', max_length=100)
+    price = models.PositiveSmallIntegerField('Цена', default=0)
+    article = models.CharField('Артикул', max_length=20, unique=True)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to="product/")
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+class RatingStar(models.Model):
+    value = models.PositiveSmallIntegerField('Значение', default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = 'Звезда рейтинга'
+        verbose_name_plural = 'Звезды рейтинга'
+
+
+class Rating(models.Model):
+    ip = models.CharField('IP адрес', max_length=20)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='Звезда')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар')
+
+    def __str__(self):
+        return f'{self.star} - {self.product}'
+
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
+
+
+class Reviews(models.Model):
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Текст", max_length=1000)
+    parent = models.ForeignKey('self', verbose_name='Родитель', blank=True, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.product}'
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'

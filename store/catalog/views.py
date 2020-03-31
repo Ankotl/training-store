db4dc358af360234ae3c62ba3935bbabd0d786c9
anton_kotl <llluzuk@mail.ru>
2020-03-31 09:12:26 +0300

    add Products view

--- store/catalog/views.py
+++ store/catalog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-
+from .models import Product
 # Create your views here.
-def index(requests):
-    return render(requests, 'index.html')
+
+
+class ProductView(View):
+    def get(self,request):
+        products = Product.objects.all()
+        return render(request, 'products/products_list.html', {'product_list': products})

53ce2279d1e923a2f0d53cd050f50ea00bbde68e
anton_kotl <llluzuk@mail.ru>
2020-04-01 11:51:51 +0300

    refactoring admin.py

--- store/catalog/admin.py
+++ store/catalog/admin.py
@@ -2,8 +2,44 @@ from django.contrib import admin
 from .models import Product, Category, Reviews, RatingStar, Rating
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Reviews)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ("id", "name")
+    list_display_links = ("name",)
+
+
+class ReviewInline(admin.TabularInline):
+    """Отзывы на странице фильма"""
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    list_display = ("name", "email", "parent", "product", "id")
+    readonly_fields = ("name", "email")
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Рейтинг"""
+    list_display = ("product", "ip")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Товары"""
+    list_display = ("name", "category", "article",)
+    list_filter = ("category",)
+    search_fields = ("name", "category__name")
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+
+
 admin.site.register(RatingStar)
-admin.site.register(Rating)
+

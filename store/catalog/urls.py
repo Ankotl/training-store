4a31f4149d8b856ccb6bd573453e021b171d2fa9
Ankotl <62921493+Ankotl@users.noreply.github.com>
2020-05-18 09:57:36 +0300

    add login logout

--- store/catalog/urls.py
+++ store/catalog/urls.py
@@ -1,18 +1,5 @@
-"""store URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from . import views
 from django.urls import include, path
 
@@ -21,5 +8,6 @@ urlpatterns = [
     path('filter/', views.FilterProductView.as_view(), name='filter'),
     path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
     path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
-    path('review/<slug:slug>/', views.AddReview.as_view(), name='add_review')
+    path('review/<slug:slug>/', views.AddReview.as_view(), name='add_review'),
+    path('accounts/', include('django.contrib.auth.urls')),
     ]
--- store/store/settings.py
+++ store/store/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+LOGIN_REDIRECT_URL = '/'
+
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
